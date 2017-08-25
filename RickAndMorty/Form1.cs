a43e5a7b11aa2369feae96237af931befe9040fc
Tor Smedberg <tor.smedberg@gmail.com>
2017-08-25 22:15:42 +0200

    Uploaded source code.

---
+++ RickAndMorty/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace RickAndMorty
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Get Schwifty!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Tiny Rick!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Wubba Dubba Dub Dub");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Get Schwifty In Here!");
+        }
+    }
+}
